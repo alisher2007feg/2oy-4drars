3e43aa2ac775a98f41a76d52638c5db378bfdbd7
alisher2007feg <113277011+alisher2007feg@users.noreply.github.com>
2022-11-08 05:57:21 +0500

    finished

--- script.js
+++
@@ -1,6 +0,0 @@
-let body = document.querySelector("body");
-let modeBtn = document.querySelector(".mode-btn");
-
-modeBtn.addEventListener('click', function(){
-    body.classList.toggle('body-dark')
-})
